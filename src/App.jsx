6d01e1aa31c5f374c5e1c7efed4afc743206350c
Martín Rodríguez <neomartinr@gmail.com>
2026-08-31 20:42:00 -0300

    Refactor App component to include product details and update Title component props for better flexibility

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import Title from './components/Title'
 
 export default function App() {
 
-  const nombre = "Juancito Perez"
+  const producto = {
+    name: "Producto 1",
+    price: 1000,
+    description: "Descripción del producto",
+    stock: 50,
+    category: "Electrónica",
+    imageUrl: "https://via.placeholder.com/150"
+  }
 
   //Logica de la función
   // ...
@@ -12,7 +19,11 @@ export default function App() {
   return (
     <>
 
-      <Title principal="Titulo principal de mi aplicación" secundario="Prop secundaria" />
+      <Title tituloPrincipal="Titulo principal de mi aplicación"
+        subtitulo="Subtitulo de mi aplicación"
+        cualquierCosa="Cualquier cosa"
+        propiedadComponenteProducto={producto} />
+
 
       <p>Bienvenido a mi aplicación de React.</p>
 
@@ -21,14 +32,19 @@ export default function App() {
       <h2>Sección de contenido</h2>
       <p>Este es el contenido de la sección.</p>
 
-      <img src="algo" alt="algo" />
-
-      <Title principal="Acerca de" />
-
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam recusandae expedita corporis perspiciatis dolore inventore cupiditate, reiciendis non, facere fugit obcaecati consequatur animi rerum optio pariatur iste dolores earum. Enim labore eaque ipsam maiores deleniti? Provident, eum? Odit ab, dolores, voluptatibus accusantium provident nam harum beatae a excepturi quae enim dicta eos ea totam? Magni nobis praesentium minima quas. Magni quaerat nobis perferendis, atque eligendi sint est ullam distinctio ab? Tenetur iusto nisi illo consequuntur dolore perferendis qui at delectus officiis consectetur error, hic molestias nostrum itaque possimus in optio ipsam cum? Non, voluptatibus ipsam ipsum reiciendis illo a corporis!</p>
 
 
-      <Title principal="Tercer uso de mi title" />
+      <Title tituloPrincipal="Acerca de" />
+
+
+
+
+
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam recusandae expedita corporis perspiciatis dolore inventore cupiditate, reiciendis non, facere fugit obcaecati consequatur animi rerum optio pariatur iste dolores earum. oluptatibus ipsam ipsum reiciendis illo a corporis!</p>
+
+
+      <Title tituloPrincipal="Tercer uso de mi title" subtitulo="Subtitulo del tercer uso" type="danger" />
 
       <p>Este es otro párrafo de ejemplo en la aplicación de React.</p>
 
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,18 +1,40 @@
 import "./Title.css";
 
-function Title(props) {
+function Title({ tituloPrincipal, subtitulo, type }) {
+
+
+  let tituloClase = "title-container";
+
+  if (type) {
+    tituloClase += ` titulo-${type}`;  // .titulo-success    .titulo-danger .titulo-info
+  }
 
   return (
-    <div className="title-container">
 
 
-      <h1 className="title">{props.principal}</h1>
+    <div className={tituloClase}>
 
+      {/* <div className={`titulo-container ${props.type ? "titulo-" + props.type : ""}`}> */}
 
-
+      <h1 className="title">{tituloPrincipal}</h1>
 
       <hr />
-      <p className="subtitle">Esta es una aplicación de ejemplo en React.</p>
+
+      {
+
+        subtitulo && <p className="subtitle">{subtitulo}</p>
+
+      }
+
+      {/* Pintar de forma condicional la imagen del producto: */}
+      {/* {
+        props.producto?.image ? <img src={producto.image} alt="" />
+
+          : <div className="image-no-provista">SIN IMAGE</div>
+      } */}
+
+
+
       <img className="title-image" src="https://i.pinimg.com/originals/e0/ef/d4/e0efd4434e673b3b146e5d06817dd8ac.png" alt="" />
     </div>
   )
